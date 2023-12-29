808c42ee524c5f697f5d4311f3358d35a28c048e
Wonjun <wonwonjun@gmail.com>
2023-12-29 20:35:27 +0900

    Initial work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,324 @@
-import Image from 'next/image'
+import Link from "next/link";
+import BrandLogo from "./components/BrandLogo";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="font-pretendard">
+      <nav className=" border-b py-[6px] border-b-grey-100">
+        <div className="flex justify-between max-w-[1200px] m-auto">
+          <Link href="/" className="flex items-center gap-2">
+            <BrandLogo />
+            <span className="text-primary-2-300 font-bold font-sourceSansPro text-xl">
+              ChakChak
+            </span>
+          </Link>
+          <Link
+            href="/to-our-register-link"
+            className="bg-grey-900 text-white px-4 py-[10px] rounded-lg"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            사전 예약 신청
+          </Link>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="flex flex-col items-center">
+        <h2 className="font-bold text-[56px] leading-[68px] mt-[120px]">
+          예약시스템, 내 웹사이트에{" "}
+          <span className="text-primary-2-300">착!</span>
+        </h2>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p className="mt-6 text-grey-500 text-xl">
+          <span className="text-primary-2-300">
+            팝업스토어, 이벤트, 매장 예약
+          </span>{" "}
+          등 각종 온, 오프라인 행사를 <br /> 내 웹사이트에서 직접 관리하세요.
+        </p>
+
+        <Link
+          href="/"
+          className="bg-grey-900 text-white flex items-center w-fit text-2xl px-6 py-4 rounded-xl mt-8"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          사전 예약 신청{" "}
+          <div>
+            <Image
+              priority
+              src={"/rightArrow.svg"}
+              alt="Right Arrow"
+              height={24}
+              width={24}
+            />
+          </div>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="mt-8">
+          <Image
+            src="/home/topSectionImage1.jpg"
+            width={1200}
+            height={584}
+            alt="Example Image"
+          />
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <section className="flex flex-col items-center">
+        <h2 className="text-[56px] leading-[72px] font-bold tracking-[-0.84px] mt-[120px] text-center">
+          이벤트 예약, 내 웹사이트에서 <br />
+          <span className="text-primary-2-300">직접 받으세요</span>
+        </h2>
+        <p className="text-grey-500 text-xl font-medium text-center mt-6">
+          착착은 2가지 <span className="text-primary-2-300">Widget SDK</span>를
+          제공하여
+          <br /> 나에게 맞는 솔루션으로
+          <br /> 이벤트를 직접 관리할 수 있어요.
+        </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <figure className="bg-grey-50 flex flex-row-reverse mt-14 rounded-3xl">
+          <Image
+            src="/home/featureImage1.png"
+            width={636}
+            height={466}
+            alt="Feature Image1"
+            className="mt-[52px] ml-[52px] mr-[52px]"
+          />
+          <figcaption className="p-6 min-w-[460px]">
+            <span className="font-semibold text-lg leading-[22px] rounded-lg bg-[#e6e2ff] px-3 py-2 inline-block text-[#3D02E3]">
+              위젯 UI
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <h3 className="font-bold text-[32px] leading-[46px] my-4">
+              내 웹사이트 어디서든 <br />
+              브랜드 이벤트를 홍보하세요
+            </h3>
+
+            <p className="text-grey-500 text-lg leading-[30px] font-medium">
+              착착에서 제공하는 Widget SDK를 통해
+              <br /> 내 웹사이트 어디서든 이벤트 예약 기능을 <br />
+              쉽고 간단하게 추가할 수 있어요.
+            </p>
+          </figcaption>
+        </figure>
+
+        <figure className="bg-grey-50 flex flex-row-reverse mt-14 rounded-3xl overflow-hidden">
+          <div className="relative min-w-[740px]">
+            <Image
+              src="/home/feature2Image1.png"
+              width={436}
+              height={675}
+              alt="Feature Image2 Image 1"
+              className="mt-[52px] ml-[52px] mr-[52px]"
+            />
+            <Image
+              src="/home/feature2Image2.png"
+              alt="feature1 image2"
+              width={436}
+              height={675}
+              className="absolute top-[26px] right-[52px]"
+            />
+          </div>
+          <figcaption className="p-6 min-w-[460px] mb-[120px]">
+            <span className="font-semibold text-lg leading-[22px] rounded-lg bg-[#e6e2ff] px-3 py-2 inline-block text-[#3D02E3]">
+              예약페이지
+            </span>
+            <h3 className="font-bold text-[32px] leading-[46px] my-4">
+              웹사이트, 개발자가 없어도 <br />
+              문제없어요
+            </h3>
+
+            <p className="text-grey-500 text-lg leading-[30px] font-medium">
+              착착이 제공하는 <br />
+              기본 예약 페이지와 링크를 생성해서 <br />
+              간단하게 고객을 모집할 수 있어요.
+            </p>
+          </figcaption>
+        </figure>
+      </section>
+      <section className="flex flex-col items-center">
+        <h2 className="text-[56px] leading-[72px] font-bold tracking-[-0.84px] mt-[120px] text-center">
+          다양한 커스터마이징으로 <br /> 브랜드를{" "}
+          <span className="text-primary-2-300">빛내보세요.</span>
+        </h2>
+
+        <p className="text-center text-xl font-medium text-grey-500 mt-6">
+          착착에서{" "}
+          <span className="text-primary-2-300">
+            브랜드 로고, 이미지, 프라이머리 컬러
+          </span>
+          를 <br /> 활용해 예약 페이지 및 티켓 UI를 <br />
+          쉽게 커스텀 할 수 있습니다.
+        </p>
+
+        <figure className="bg-grey-50 flex flex-row-reverse mt-14 rounded-3xl overflow-hidden">
+          <div className="relative min-w-[740px]">
+            <Image
+              src="/home/ticket1.png"
+              width={254}
+              height={505}
+              alt="Feature Image3 Ticket Image"
+              className="absolute bottom-0 left-[120px]"
+            />
+            <Image
+              src="/home/ticket2.png"
+              alt="feature3 ticket Image2"
+              width={254}
+              height={505}
+              className="absolute top-0 right-[86px]"
+            />
+          </div>
+          <figcaption className="p-6 min-w-[460px] mb-[120px]">
+            <span className="font-semibold text-lg leading-[22px] rounded-lg bg-[#e6e2ff] px-3 py-2 inline-block text-[#3D02E3]">
+              브랜드티켓
+            </span>
+            <h3 className="font-bold text-[32px] leading-[46px] my-4">
+              예약 티켓을 브랜드 경험의 <br />
+              새로운 창구로 활용해 보세요.
+            </h3>
+
+            <p className="text-grey-500 text-lg leading-[30px] font-medium">
+              착착은 참석 확인을 위한 예약 티켓을 생성 가능해요.
+              <br />
+              티켓에 브랜드 이미지를 삽입하고, <br />
+              고객들에게 더욱 강력한 브랜드 인상을 남겨보세요.
+              <br />
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-grey-50 flex flex-row-reverse mt-14 rounded-3xl overflow-hidden">
+          <div className="relative min-w-[740px]">
+            <Image
+              src="/home/reservationPage.png"
+              width={590}
+              height={428}
+              alt="Feature Image3 Ticket Image"
+              className="mt-[52px] ml-[52px]"
+            />
+
+            <div className=" bg-white rounded-[12px] shadow-custom p-6 w-fit absolute bottom-[24px] right-[52px]">
+              <div className="px-6 py-4 bg-gradient-to-r from-[#ff00f5] to-[#ff5c00] rounded-[12px] text-white font-bold text-2xl w-[338px] flex justify-center">
+                예약하기
+              </div>
+            </div>
+          </div>
+          <figcaption className="p-6 min-w-[460px] mb-[120px]">
+            <span className="font-semibold text-lg leading-[22px] rounded-lg bg-[#e6e2ff] px-3 py-2 inline-block text-[#3D02E3]">
+              UI커스텀
+            </span>
+            <h3 className="font-bold text-[32px] leading-[46px] my-4">
+              브랜드를 담은 예약 페이지, <br />
+              착착해서 만들어보세요.
+            </h3>
+
+            <p className="text-grey-500 text-lg leading-[30px] font-medium">
+              착착의 기본 예약 페이지는
+              <br />
+              브랜드의 프라이머리 컬러, 이미지를 적용할 수 있어요.
+              <br />
+              브랜드의 이야기를 쉽게 전달해 보세요.
+              <br />
+            </p>
+          </figcaption>
+        </figure>
+      </section>
+      <section className="flex flex-col items-center my-[120px]">
+        <h2 className="text-[56px] leading-[72px] font-bold tracking-[-0.84px] text-center">
+          예약 관리와 고객 CRM도 <br /> 착착!
+          <span className="text-primary-2-300">관리하세요.</span>
+        </h2>
+
+        <p className="text-center text-xl font-medium text-grey-500 mt-6">
+          직관적인 대시보드로 예약자 관리와 <br />
+          고객 CRM을 간편하게 관리하고 <br />
+          <span className="text-primary-2-300">마케팅 기회로 전환하세요.</span>
+        </p>
+
+        <figure className="bg-grey-50 flex flex-row-reverse mt-14 rounded-3xl overflow-hidden max-h-[480px]">
+          <div className="relative min-w-[740px]">
+            <Image
+              src="/home/reservationManagePage.png"
+              width={450}
+              height={300}
+              alt="Feature Image3 Ticket Image"
+              className="mt-[52px]"
+            />
+            <Image
+              src="/home/ticket3.png"
+              width={254}
+              height={505}
+              alt="ticket"
+              className="absolute top-0 right-[52px]"
+            />
+          </div>
+          <figcaption className="p-6 min-w-[460px] mb-[120px]">
+            <span className="font-semibold text-lg leading-[22px] rounded-lg bg-[#e6e2ff] px-3 py-2 inline-block text-[#3D02E3]">
+              예약자 관리
+            </span>
+            <h3 className="font-bold text-[32px] leading-[46px] my-4">
+              온, 오프라인 예약자 관리도 <br />
+              간단하게 할 수 있어요
+            </h3>
+
+            <p className="text-grey-500 text-lg leading-[30px] font-medium">
+              예약 티켓의 QR 코드로 예약자를 자동으로 인식하며,
+              <br />
+              상태 창을 이용하여 수동으로
+              <br />
+              예약자를 편리하게 관리할 수 있어요.
+              <br />
+            </p>
+          </figcaption>
+        </figure>
+
+        <figure className="bg-grey-50 flex flex-row-reverse mt-14 rounded-3xl overflow-hidden">
+          <div className="relative min-w-[740px]">
+            <Image
+              src="/home/crmFeature.png"
+              width={740}
+              height={480}
+              alt="CRM feature description Image"
+              className=""
+            />
+          </div>
+          <figcaption className="p-6 min-w-[460px] mb-[120px]">
+            <span className="font-semibold text-lg leading-[22px] rounded-lg bg-[#e6e2ff] px-3 py-2 inline-block text-[#3D02E3]">
+              CRM 데이
+            </span>
+            <h3 className="font-bold text-[32px] leading-[46px] my-4">
+              이벤트 마케팅 효과를
+              <br />
+              한눈에 파악하세요
+            </h3>
+
+            <p className="text-grey-500 text-lg leading-[30px] font-medium">
+              일별 예약자 상태, 시간대별 참석 비율,
+              <br />
+              성별 분포 등 다양한 CRM 데이터를
+              <br />
+              그래프로 한누에 볼 수 있어요.
+              <br />
+            </p>
+          </figcaption>
+        </figure>
+      </section>
+
+      <section className="flex flex-col items-center bg-grey-800 py-[120px]">
+        <h2 className="text-center text-white text-[44px] leading-[64px] font-bold ">
+          지금 <span className="text-primary-2-300">착착</span> 사전예약하고{" "}
+          <br />
+          베타테스터로 가장 먼저 이용해 보세요.
+        </h2>
+
+        <Link
+          href=""
+          className="flex text-white text-2xl font-bold bg-primary-2-300 rounded-[12px] px-6 py-4 mt-14"
+        >
+          사전 예약 신청{" "}
+          <Image
+            src="/rightArrow.svg"
+            alt="right arrow"
+            height={24}
+            width={24}
+          />
+        </Link>
+      </section>
     </main>
-  )
+  );
 }
