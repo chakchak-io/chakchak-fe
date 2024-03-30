ba67e5ccd213945e7a026cd79c3991eef0f8ab8d
woodi97 <woodi.daily@gmail.com>
2024-03-30 16:33:53 +0900

    fix: resolve nextjs build error(wrong layout export)

--- src/app/channel/[channelName]/event/[eventId]/layout.tsx
+++ src/app/channel/[channelName]/event/[eventId]/layout.tsx
@@ -1,21 +1,19 @@
-import { NextPage } from 'next';
-import { PropsWithChildren } from 'react';
-
 import { AppLayout } from '@/components/layout';
 import { Container } from '@/components/ui/container';
 import { Flex } from '@/components/ui/flex';
 import { ChannelName, EventId } from '@/const/router';
 import { typedRedirect } from '@/lib/nextjs/server-navigation';
-import { CommonNextPageProps } from '@/lib/nextjs/type';
 
-const EventDashboardLayout: NextPage<
-  PropsWithChildren<
-    CommonNextPageProps<{
-      channelName: ChannelName;
-      eventId: EventId;
-    }>
-  >
-> = ({ children, params: { channelName, eventId } }) => {
+export default function EventPageLayout({
+  children,
+  params: { channelName, eventId },
+}: {
+  children: React.ReactNode;
+  params: {
+    channelName: ChannelName;
+    eventId: EventId;
+  };
+}) {
   if (!channelName || !eventId) {
     typedRedirect('/channel');
     return null;
@@ -30,6 +28,4 @@ const EventDashboardLayout: NextPage<
       </Flex>
     </main>
   );
-};
-
-export default EventDashboardLayout;
+}
