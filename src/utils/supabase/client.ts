02d8716a2f097f810305bebf230293ecb43c3ca1
woodi97 <woodi.daily@gmail.com>
2024-05-11 14:31:42 +0900

    chore: apply typesafe env to supabase client create function

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,10 +1,11 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 import { Database } from '~/database.types';
+import { env } from '~/env.mjs';
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   );
 }
