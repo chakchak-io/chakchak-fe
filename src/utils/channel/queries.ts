d470415c2a95fbb3ef487d1560061825b88c70be
WonjunJang <wonwonjun@gmail.com>
2024-05-11 13:07:21 +0900

    feature: Fetch Channels complete (#20)

--- src/utils/channel/queries.ts
+++ src/utils/channel/queries.ts
@@ -6,3 +6,7 @@ export const getChannelCategories = async (client: TypedSupabaseClient) =>
 
 export const createChannel = async (client: TypedSupabaseClient, data: ChannelCreateData) =>
   await client.from('channel').insert(data).throwOnError();
+
+// we fetch channels that were created by the user
+export const fetchChannels = async (client: TypedSupabaseClient, owner_id: string) =>
+  await client.from('channel').select('*').eq('owner', owner_id);
